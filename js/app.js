db18dde4d10618ca18252502a059fe0973b8b173
Jekain <payback228@gmail.com>
2019-05-20 10:29:45 +0300

    'fixAndAddJS'

---
+++ js/app.js
@@ -0,0 +1,55 @@
+$(function(){
+
+
+	var introH = $("#intro").innerHeight(),
+		header = $("#header"),
+		scrollOffset = $(window).scrollTop();
+
+	checkScroll(scrollOffset);
+
+	//fixedHeader
+	$(window).on("scroll", function(){
+
+		scrollOffset = $(this).scrollTop();
+		checkScroll(scrollOffset);
+
+	});
+
+	function checkScroll(scrollOffset) {
+		
+		if (scrollOffset >= introH) {
+			header.addClass("intro--fixed");
+		} else{
+			header.removeClass("intro--fixed");
+		}
+	}
+
+
+	//smooth scroll
+
+	$("[data-scroll]").on("click", function(event){
+		event.preventDefault();
+
+		var $this = $(this),
+			blockId = $this.data('scroll'),
+			blockOffset = $(blockId).offset().top;
+
+		$("#nav a").removeClass("active");
+		$this.addClass("active");
+
+		$("html, body").animate({
+			scrollTop: blockOffset
+		}, 500);
+
+	});
+
+	//menu Nav
+
+	$("#nav_toggle").on("click",function(event){
+		event.preventDefault();
+
+		$(this).toggleClass("active");
+		$("#nav").toggleClass("active");
+
+	})
+})
